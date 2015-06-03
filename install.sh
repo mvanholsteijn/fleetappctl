2ea2f80f5a544eebc5539dda9dbf8ddcbe76c49e
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-06-03 08:29:50 +0200

    added consul key value deployment

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ else
 	echo "INFO: copying fleetappctl to /usr/local/bin" >&2
 	cp bin/fleetappctl /usr/local/bin
 fi
+
+if cmp {/usr/local,.}/bin/consul.KeyValuePairs 2> /dev/null ; then
+	echo "INFO: consul.KeyValuePairs already installed in /usr/local/bin" >&2
+else
+	echo "INFO: copying consul.KeyValuePairs to /usr/local/bin" >&2
+	cp bin/consul.KeyValuePairs /usr/local/bin
+fi
